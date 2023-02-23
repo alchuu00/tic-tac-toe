633f9436d5fdd901ba7c9452bed408f37581060b
Alja Čekada <alja.cekada@gmail.com>
2023-02-23 20:45:52 +0100

    update game logic

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const gameBoard = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
-
 function createPlayer(name, symbol) {
   return {
     name: name,
     symbol: symbol,
   };
 }
-let player1 = createPlayer('Player 1', 'X');
-let player2 = createPlayer('Player 2', 'O');
+
+const createGameBoard = () => {
+  return [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+};
 
 const getRow = () => {
   return parseInt(prompt('input row number'));
@@ -21,16 +21,61 @@ const getColumn = () => {
   return parseInt(prompt('input column number'));
 };
 
-const updateBoard = (row, col) => {
-  gameBoard[row][col] = currentPlayer.symbol;
+const updateBoard = (row, col, currentPlayer, gameboard) => {
+  // copy the gameboard
+  const gameboardCopy = Object.assign({}, gameboard); // make a copy of an object
+  // modify the copy
+  gameboardCopy[row][col] = currentPlayer.symbol;
+  // return the copy
+  return gameboardCopy;
 };
 
-const switchPlayer = () => {
+const getNextPlayer = (currentPlayer, player1, player2) => {
   if (currentPlayer === player1) {
-    currentPlayer = player2;
+    return player2;
   } else {
-    currentPlayer = player1;
+    return player1;
   }
 };
 
+(function controller() {
+  const player1 = createPlayer('Player 1', 'X');
+  const player2 = createPlayer('Player 2', 'O');
 
+  let currentPlayer = player1;
+
+  // set up initial board
+  let gameboard = createGameBoard();
+  console.log(
+    '🚀 ~ file: script.js:49 ~ controller ~ gameboard:',
+    JSON.stringify(gameboard)
+  );
+
+  // game loop
+  // 1. take a turn
+  // 2. check if there's a winner
+  // 3. if not, repeat
+  let gameOver = false;
+  debugger;
+  while (!gameOver) {
+    const newGameboard = takeATurn(gameboard, currentPlayer);
+    gameboard = newGameboard;
+    gameOver = checkForWinner();
+    currentPlayer = getNextPlayer(currentPlayer, player1, player2);
+  }
+})();
+
+function takeATurn(gameboard, currentPlayer) {
+  const newGameBoard = updateBoard(
+    getRow(),
+    getColumn(),
+    currentPlayer,
+    gameboard
+  );
+  return newGameBoard;
+}
+
+function checkForWinner() {
+  // TODO
+  return false;
+}
