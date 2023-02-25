3e8b2d580a26f6e0d8777c563624304cf6343303
Alja Čekada <alja.cekada@gmail.com>
2023-02-25 16:38:42 +0100

    Fix DOM manipulation

--- script.js
+++ script.js
@@ -5,34 +5,14 @@ function createPlayer(name, symbol) {
   };
 }
 
-const createGameboard = () => [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
-
-const getRow = (currentPlayer) => parseInt(prompt(`${currentPlayer.name} input row number`));
-
-const getColumn = (currentPlayer) => parseInt(prompt(`${currentPlayer.name} input column number`));
-
-const updateBoard = (row, col, currentPlayer, gameboard) => {
-  // copy the gameboard
-  const gameboardCopy = gameboard.map((row) => [...row]);
-  // modify the copy
-  gameboardCopy[row][col] = currentPlayer.symbol;
-
-  // return the copy
-  return gameboardCopy;
+const createGameboard = () => {
+  const squares = document.querySelectorAll('.square');
+  return Array.from(squares);
 };
 
-const takeATurn = (gameboard, currentPlayer) => {
-  const newGameboard = updateBoard(
-    getRow(currentPlayer),
-    getColumn(currentPlayer),
-    currentPlayer,
-    gameboard,
-  );
-  return newGameboard;
+const updateBoard = (square, currentPlayer) => {
+  square.textContent = currentPlayer.symbol;
+  square.setAttribute('data-symbol', currentPlayer.symbol);
 };
 
 const getNextPlayer = (currentPlayer, player1, player2) => {
@@ -42,78 +22,68 @@ const getNextPlayer = (currentPlayer, player1, player2) => {
   return player1;
 };
 
-const checkForWinner = (newGameboard) => {
+const checkForWinner = (gameboard) => {
   const winningCombinations = [
-    // Rows
-    [[0, 0], [0, 1], [0, 2]],
-    [[1, 0], [1, 1], [1, 2]],
-    [[2, 0], [2, 1], [2, 2]],
-
-    // Columns
-    [[0, 0], [1, 0], [2, 0]],
-    [[0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 2], [2, 2]],
-
-    // Diagonals
-    [[0, 0], [1, 1], [2, 2]],
-    [[0, 2], [1, 1], [2, 0]],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-debugger
-  for (let i = 0; i < winningCombinations.length; i++) {
-    const combo = winningCombinations[i];
-    const [row1, col1] = combo[0];
-    const [row2, col2] = combo[1];
-    const [row3, col3] = combo[2];
-    const square1 = newGameboard[row1][col1];
-    const square2 = newGameboard[row2][col2];
-    const square3 = newGameboard[row3][col3];
 
-    if (square1 && square1 === square2 && square1 === square3) {
-      console.log('you won');
+  // Check each winning combination to see if it's present in the gameboard
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      gameboard[a].getAttribute('data-symbol')
+      && gameboard[a].getAttribute('data-symbol')
+        === gameboard[b].getAttribute('data-symbol')
+      && gameboard[a].getAttribute('data-symbol')
+        === gameboard[c].getAttribute('data-symbol')
+    ) {
       return true;
     }
   }
-
-  // Check for a draw
-  let isDraw = true;
-  for (let i = 0; i < newGameboard.length; i++) {
-    for (let j = 0; j < newGameboard[i].length; j++) {
-      if (newGameboard[i][j] === 0) {
-        isDraw = false;
-        break;
-      }
-    }
-    if (!isDraw) {
-      break;
-    }
-  }
-  if (isDraw) {
-    console.log('The game ended in a draw');
-    return true;
-  }
-
   return false;
 };
 
-(function controller() {
+// START GAME
+(function gameController() {
   const player1 = createPlayer('Player 1', 'X');
   const player2 = createPlayer('Player 2', 'O');
 
   let currentPlayer = player1;
 
   // set up initial board
-  let gameboard = createGameboard();
+  const gameboard = createGameboard();
 
   // game loop
   // 1. take a turn
-  // 2. check if there's a winner
+  // 2. check if there's a winner or a draw
   // 3. if not, repeat
-  let gameOver = false;
+  const squares = document.querySelectorAll('.square');
+  let numTurnsTaken = 0;
+  squares.forEach((square) => {
+    square.addEventListener('click', () => {
+      if (!square.hasAttribute('data-symbol')) {
+        updateBoard(square, currentPlayer);
+        numTurnsTaken++;
 
-  while (!gameOver) {
-    const newGameboard = takeATurn(gameboard, currentPlayer);
-    gameboard = newGameboard;
-    gameOver = checkForWinner(newGameboard);
-    currentPlayer = getNextPlayer(currentPlayer, player1, player2);
-  }
+        if (checkForWinner(gameboard)) {
+          console.log(`${currentPlayer.name} wins!`);
+          return;
+        }
+
+        if (numTurnsTaken === squares.length) {
+          console.log("It's a draw!");
+          return;
+        }
+
+        currentPlayer = getNextPlayer(currentPlayer, player1, player2);
+      }
+    });
+  });
 }());
